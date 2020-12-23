b92c9a70a591001621eaccd41c53e157c505ec6a
v_fantnzeng <v_fantnzeng@tencent.com>
2020-12-23 14:01:22 +0800

    1

--- cpp_cc/AST.h
+++ cpp_cc/AST.h
@@ -1,3 +1,5 @@
+#include <string>
+
 #pragma once
 namespace tadcc {
 namespace ast {
@@ -6,6 +8,22 @@ struct TreeNode {
   virtual ~TreeNode();
 };
 
-template <typename T> struct ValueNode { T value; };
+template <typename T> struct ValueNode : public TreeNode { T value; };
+
+struct BinOpNode : public TreeNode {
+  std::string op;
+  TreeNode *left;
+  TreeNode *right;
+};
+
+struct PreOpNode : public TreeNode {
+  std::string op;
+  TreeNode *back;
+};
+
+struct SupOpNode : public TreeNode {
+  std::string op;
+  TreeNode *front;
+};
 } // namespace ast
 } // namespace tadcc
