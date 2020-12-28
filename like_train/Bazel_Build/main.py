e270d7617ddccd396da2c39806c3d4f1106e25ff
v_fantnzeng <v_fantnzeng@tencent.com>
2020-12-28 14:54:59 +0800

    1

---
+++ like_train/Bazel_Build/main.py
@@ -0,0 +1,34 @@
+import sys
+import antlr4 as antlr
+from dist.BazelBuildLexer import BazelBuildLexer
+from dist.BazelBuildParser import BazelBuildParser
+from dist.BazelBuildVisitor import BazelBuildVisitor
+
+
+class BazelVisitor(BazelBuildVisitor):
+
+    @Override
+    def 
+
+
+def main(f=None):
+    if f is None:
+        stream = antlr.StdinStream()
+    else:
+        stream = antlr.InputStream(f)
+
+    lexer = BazelBuildLexer(stream)
+    token_stream = antlr.CommonTokenStream(lexer)
+    parser = BazelBuildParser(token_stream)
+
+    tree = parser.prog()
+    vi = BazelBuildVisitor()
+    vi.visit(tree)
+
+
+if __name__ == "__main__":
+    if(len(sys.argv) > 1):
+        with open(sys.argv[1]) as f:
+            main(f.read())
+    else:
+        main()
