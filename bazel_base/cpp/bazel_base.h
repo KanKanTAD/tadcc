d03cac9f28e0fc4e284870ec54a45b81270a829c
v_fantnzeng <v_fantnzeng@tencent.com>
2020-12-28 10:49:47 +0800

    1

--- bazel_base/cpp/bazel_base.h
+++ bazel_base/cpp/bazel_base.h
@@ -8,14 +8,22 @@ namespace tadcc {
 namespace bazel_base {
 
 struct Namiable {
-  virtual std::string get_name() const { return this->name_; }
+  virtual const std::string &get_name() const { return this->name_; }
   virtual void set_name(const std::string &name) { this->name_ = name; }
 
 protected:
   std::string name_;
 };
 
-struct Value {
+struct Notiable {
+  virtual const std::string &get_note() const { return note_; }
+  virtual void set_node(const std::string &node) { this->note_ = node; }
+
+protected:
+  std::string note_;
+};
+
+struct Value : public Notiable {
   char *value = nullptr;
   int leng = 0;
   Value() {}
@@ -32,7 +40,7 @@ struct Value {
 
 extern std::ostream &operator<<(std::ostream &, const Value &);
 
-class Attribute : public Namiable {
+class Attribute : public Namiable, public Notiable {
   std::set<Value> values_;
   bool is_multi_;
 
@@ -48,7 +56,7 @@ public:
   virtual Value find_value(const std::string &) const;
 };
 
-class Target : public Namiable {
+class Target : public Namiable, public Notiable {
 public:
   virtual void list_attribute(std::list<Attribute> &attributes) const;
   virtual std::string get_rulename() const;
@@ -56,7 +64,7 @@ public:
   virtual Attribute &select_attr(const std::string &attr_name);
 };
 
-class BazelBase : public Namiable {
+class BazelBase : public Namiable ,public Notiable{
 public:
   virtual void reload(const std::string &file_path);
   virtual void list_target(std::list<Target> &targets);
