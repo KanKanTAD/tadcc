37a9c003b40d39b921cc7fcc356b5ffa543a1956
v_fantnzeng <v_fantnzeng@tencent.com>
2021-01-04 11:31:16 +0800

    1

--- bazel_base/java_export/Main.java
+++ bazel_base/java_export/Main.java
@@ -1,11 +1,10 @@
-import org.antlr.v4.runtime.*;
 
-public class Main{
-	public static void main(String[] args){
+public class Main {
+	public static void main(String[] args) {
 		String file_path = null;
-		if(args.length > 1){
+		if (args.length > 1) {
 			file_path = args[1];
 		}
-		System.out.println("hello world");
+		System.out.println(file_path);
 	}
 }
