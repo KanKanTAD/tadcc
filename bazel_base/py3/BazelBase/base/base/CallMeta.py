8516d2b5c087ceebe41a267d351b58b77ea0ff49
v_fantnzeng <v_fantnzeng@tencent.com>
2021-01-04 15:40:42 +0800

    1

--- bazel_base/py3/BazelBase/base/base/CallMeta.py
+++ bazel_base/py3/BazelBase/base/base/CallMeta.py
@@ -12,6 +12,9 @@ class CallMeta(base.Stringify.Stringify):
     def get_arglist(self: CallMeta) -> List[Argument]:
         return self.__arg_list
         
+    def set_arglist(self: CallMeta, arg_list: List[Argument]) -> void:
+        self.__arg_list = arg_list
+        
     def __init__(self):
         super(CallMeta, self).__init__()
         self.__arg_list = list()
---
+++ bazel_base/py3/BazelBase/set_arglist_body_656b258_2.py
@@ -0,0 +1,1 @@
+self.__arg_list = arg_list
