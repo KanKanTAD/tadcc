5356507c01f866e224e54856da5976f76dd8af5c
v_fantnzeng <v_fantnzeng@tencent.com>
2021-01-05 15:33:28 +0800

    2

--- bazel_base/cpp/example.cc
+++ bazel_base/cpp/example.cc
@@ -1,7 +1,34 @@
+#include "BazelBuildBaseVisitor.h"
+#include "BazelBuildLexer.h"
+#include "BazelBuildParser.h"
+#include "BazelBuildVisitor.h"
+#include "CharStream.h"
+#include "CommonTokenStream.h"
+#include <antlr4-runtime/antlr4-runtime.h>
 #include <iostream>
 
-using namespace std;
+class MyVisitor : public BazelBuildBaseVisitor {
+public:
+  antlrcpp::Any visitProg(BazelBuildParser::ProgContext *ctx) override {
+    std::cout << "hello world" << std::endl;
+    return BazelBuildBaseVisitor::visitProg(ctx);
+  }
+};
 
-int main(int argc,char* argv[]){
+int main(int argc, char *argv[]) {
 
-return 0;}
+  antlr4::CharStream *_stream = nullptr;
+  if (argc > 1) {
+    _stream = new antlr4::ANTLRFileStream(argv[1]);
+  } else {
+    _stream = new antlr4::ANTLRInputStream(std::cin);
+  }
+  BazelBuildLexer lexer(_stream);
+  antlr4::CommonTokenStream token_stream(&lexer);
+  BazelBuildParser parser(&token_stream);
+  auto tree = parser.prog();
+  MyVisitor visitor;
+  auto x = visitor.visit(tree);
+
+  delete _stream;
+}
