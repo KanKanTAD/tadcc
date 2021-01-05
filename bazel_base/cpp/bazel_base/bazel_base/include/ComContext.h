96c4d8d8b60c98c4f234dee9dfa58e77ba11cab2
v_fantnzeng <v_fantnzeng@tencent.com>
2021-01-05 17:27:06 +0800

    2

--- bazel_base/cpp/bazel_base/bazel_base/include/ComContext.h
+++ bazel_base/cpp/bazel_base/bazel_base/include/ComContext.h
@@ -1,55 +1,54 @@
 #ifndef BAZEL_BASE_COMCONTEXT_H
 #define BAZEL_BASE_COMCONTEXT_H
 
-
-#include <map>
-using namespace std;
 #include <atomic>
+#include <map>
+#include <tuple>
+#include <utility>
 using namespace std;
 
-namespace bazel_base { class Com; } 
+namespace bazel_base {
+class Com;
+}
 
 namespace bazel_base {
 
 class ComContext {
-  public:
-    static ComContext & instance();
+public:
+  static ComContext &instance();
 
+private:
+  explicit ComContext();
 
-  private:
-    explicit ComContext();
+public:
+  virtual ~ComContext();
 
+private:
+  ComContext(ComContext &source);
 
-  public:
-    virtual ~ComContext();
+  ComContext(const ComContext &source);
 
+  ComContext &operator=(ComContext &source) = delete;
 
-  private:
-    ComContext(ComContext & source);
+  ComContext &operator=(const ComContext &source) = delete;
 
-    ComContext(const ComContext & source);
+public:
+  virtual long gen_id() const;
 
-    ComContext & operator =(ComContext & source) = delete;
+  template <class T> T *make_com() const {
+    auto o = new T;
+    this->con_.insert(std::make_pair(o->get_id(), (Com *)o));
+    return o;
+  }
 
-    ComContext & operator =(const ComContext & source) = delete;
+  void _release(long id) const;
 
+  void _release(Com *obj) const;
 
-  public:
-    virtual long gen_id() const;
+private:
+  mutable map<long, Com *> con_;
 
-    template<class T>
-    T * make_com() const;
-
-    void _release(long id) const;
-
-    void _release(Com * obj) const;
-
-
-  private:
-    mutable map<long,Com*> con_;
-
-    mutable atomic_long id_inc_;
-
+  mutable atomic_long id_inc_;
 };
 
 } // namespace bazel_base
--- bazel_base/cpp/bazel_base/bazel_base/src/ComContext.cpp
+++ bazel_base/cpp/bazel_base/bazel_base/src/ComContext.cpp
@@ -4,17 +4,15 @@
 
 namespace bazel_base {
 
-ComContext & ComContext::instance()
-{
+ComContext &ComContext::instance() {
 
   static ComContext inst_;
   return inst_;
 }
 
-ComContext::ComContext(){
-}
+ComContext::ComContext() {}
 
-ComContext::~ComContext(){
+ComContext::~ComContext() {
 
   for (auto [id, obj] : this->con_) {
     delete obj;
@@ -22,17 +20,7 @@ ComContext::~ComContext(){
   this->con_.clear();
 }
 
-long ComContext::gen_id() const {
-return id_inc_.fetch_add(1);
-}
-
-template<class T>
-T * ComContext::make_com() const {
-
-  auto o = new T();
-  this->con_.insert({o->get_id(), o});
-  return o;
-}
+long ComContext::gen_id() const { return id_inc_.fetch_add(1); }
 
 void ComContext::_release(long id) const {
 
@@ -44,13 +32,12 @@ void ComContext::_release(long id) const {
   this->con_.erase(it);
 }
 
-void ComContext::_release(Com * obj) const {
+void ComContext::_release(Com *obj) const {
 
   if (obj == nullptr) {
     return;
   }
   _release(obj->get_id());
 }
-
 
 } // namespace bazel_base
