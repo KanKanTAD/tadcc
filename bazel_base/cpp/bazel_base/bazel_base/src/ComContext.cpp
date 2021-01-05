949362b45d70e41928ab77b798e5baa2024cc225
v_fantnzeng <v_fantnzeng@tencent.com>
2021-01-05 17:18:17 +0800

    2

--- bazel_base/cpp/bazel_base/bazel_base/src/ComContext.cpp
+++ bazel_base/cpp/bazel_base/bazel_base/src/ComContext.cpp
@@ -4,17 +4,15 @@
 
 namespace bazel_base {
 
-ComContext & ComContext::instance()
-{
+ComContext &ComContext::instance() {
 
   static ComContext inst_;
   return inst_;
 }
 
-ComContext::ComContext(){
-}
+ComContext::ComContext() {}
 
-ComContext::~ComContext(){
+ComContext::~ComContext() {
 
   for (auto [id, obj] : this->con_) {
     delete obj;
@@ -22,19 +20,16 @@ ComContext::~ComContext(){
   this->con_.clear();
 }
 
-long ComContext::gen_id() const {
-return id_inc_.fetch_add(1);
-}
+long ComContext::gen_id() const { return id_inc_.fetch_add(1); }
 
-template<class T>
-T * ComContext::make_com() const {
+template <class T> T *ComContext::make_com() const {
 
   auto o = new T();
   this->con_.insert({o->get_id(), o});
   return o;
 }
 
-void ComContext::_release() const {
+void ComContext::_release(long id) const {
 
   auto it = this->con_.find(id);
   if (it == this->con_.end()) {
@@ -44,13 +39,12 @@ void ComContext::_release() const {
   this->con_.erase(it);
 }
 
-void ComContext::_release() const {
+void ComContext::_release(Com *obj) const {
 
   if (obj == nullptr) {
     return;
   }
   _release(obj->get_id());
 }
-
 
 } // namespace bazel_base
